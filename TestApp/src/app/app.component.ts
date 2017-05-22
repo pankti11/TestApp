7d6ba125e9e27fa36b7c17a73f2088237e3effa1
Bhalani <pankti.bhalani@philips.com>
2017-05-22 16:01:16 -0400

    changes

--- TestApp/src/app/app.component.ts
+++ TestApp/src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { Http } from '@angular/http';
 import { DataService } from "./services/DataService";
 import { Inject } from "@angular/core";
-import { TodoItem } from "./TodoItem";
 import { RouterModule } from '@angular/router';
 
 @Component({
